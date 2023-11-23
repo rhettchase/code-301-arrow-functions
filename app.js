d1475559e2a1a6648e0a9ec5e0daee72ea01cd23
RHETT CHASE <rhettjchase@gmail.com>
2023-11-23 13:32:49 -0800

    completed steps 1-3

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const theOldWay = function(course) {
   return `I am currently enrolled in ${course}`;
 };
 
-// TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('The old way:', theOldWay('Code 301'));
+// DONE: Uncomment the following line of code to see the output in the browser console
+console.log('The old way:', theOldWay('Code 301'));
 
 
 // STEP 2
@@ -17,8 +17,8 @@ const theNewWay = (course) => {
   return `I am currently enrolled in ${course}`;
 };
 
-// TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('The new way:', theNewWay('Code 301'));
+// DONE: Uncomment the following line of code to see the output in the browser console
+console.log('The new way:', theNewWay('Code 301'));
 
 
 // STEP 3
@@ -27,8 +27,8 @@ const withoutParens = course => {
   return `I am currently enrolled in ${course}`;
 };
 
-// TODO: Uncomment the following line of code to see the output in the browser console
-// console.log('Without parens:', withoutParens('Code 301'));
+// DONE: Uncomment the following line of code to see the output in the browser console
+console.log('Without parens:', withoutParens('Code 301'));
 
 
 // STEP 4
